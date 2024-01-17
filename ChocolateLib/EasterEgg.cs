b0e48deca306481f5d942e23c2c3bb9adff2c2a1
Oliver Mikkel Opfermann Göth <oliv8037@edu.zealand.dk>
2024-01-17 17:03:04 +0100

    Add project files.

---
+++ ChocolateLib/EasterEgg.cs
@@ -0,0 +1,15 @@
+﻿namespace ChocolateLib
+{
+    public class EasterEgg
+    {
+        public int ProductNO { get; set; }
+        public string ChocolateType { get; set; }
+        public int Price { get; set; }
+        public int InStock { get; set; }
+
+        public override string ToString()
+        {
+            return "Product Number: " + ProductNO + ", Type: " + ChocolateType + ", Price: " + Price + ", In stock: " + InStock;
+        }
+    }
+}
---
+++ ChocolateLib/EasterEggsRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocolateLib
+{
+    public class EasterEggsRepository
+    {
+        private int _productNo = 1;
+        private readonly List<EasterEgg> easterEggs = new List<EasterEgg>();
+
+        public EasterEggsRepository()
+        {
+            easterEggs.Add(new EasterEgg() { ProductNO = _productNo++, ChocolateType = "Hvid", Price = 100, InStock = 1000 });
+            easterEggs.Add(new EasterEgg() { ProductNO = _productNo++, ChocolateType = "Lys", Price = 200, InStock = 1000 });
+            easterEggs.Add(new EasterEgg() { ProductNO = _productNo++, ChocolateType = "Mørk", Price = 200, InStock = 1000 });
+        }
+
+        public EasterEgg GetAll()
+        {
+            return new List<EasterEgg>(easterEggs);
+        }
+
+        public EasterEgg? GetByProductNo(int productNo)
+        {
+            return easterEggs.FirstOrDefault(e => e.ProductNO == productNo);
+        }
+
+        public void Update(EasterEgg easterEgg, int productNo)
+        {
+            EasterEgg? ee = GetByProductNo(productNo);
+            if (ee == null)
+            {
+                throw new ArgumentOutOfRangeException("Easteregg is not found");
+            }
+            ee.ProductNO = easterEgg.ProductNO;
+            ee.ChocolateType = easterEgg.ChocolateType;
+            ee.Price = easterEgg.Price;
+            ee.InStock = easterEgg.InStock;
+        }
+    }
+}
